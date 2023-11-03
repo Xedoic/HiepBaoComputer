c0e715ef905afcf1e4c9d935493c6b7ef766c658
buing@AnhDell <buinguyenvie@gmail.con>
2023-11-04 00:26:26 +0700

    first commit

---
+++ HiepBaoComputer/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HiepBaoComputer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HiepBaoComputer/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using HiepBaoComputer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiepBaoComputer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HiepBaoComputerContext _context;
+
+        public HomeController(HiepBaoComputerContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var hiepBaoComputerContext = _context.SanPhams.Include(s => s.MaDmNavigation);
+            return View(await hiepBaoComputerContext.ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
